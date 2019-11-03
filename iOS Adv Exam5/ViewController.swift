7d3431e736b7f6fff8cf02e2746fe14e34fe6173
peterpan <apppeterpan@gmail.com>
2019-11-03 18:03:03 +0800

    Initial Commit

---
+++ iOS Adv Exam5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Adv Exam5
+//
+//  Created by SHIH-YING PAN on 2019/11/3.
+//  Copyright © 2019 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
